19eb1842f6a878f2d8735bc1870878bb071b79d1
plasticine <975036719@qq.com>
2024-04-05 21:45:15 +0800

    feat(shared): add shared props - SharedColorProp

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,4 +2,7 @@
 export { clsx } from 'clsx'
 
 // export shared utilities
-export * from './class-name'
+export * from './utilities'
+
+// export shared types
+export type * from './types'
---
+++ packages/shared/src/types/props.ts
@@ -0,0 +1,4 @@
+/**
+ * The color prop shared with components.
+ */
+export type SharedColorProp = 'default' | 'primary' | 'secondary' | 'success' | 'warning' | 'danger'
