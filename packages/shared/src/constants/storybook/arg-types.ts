921709b2271d4eb1d416315271670bbfe6361d7a
plasticine <975036719@qq.com>
2024-04-05 23:09:38 +0800

    feat(shared): add shared storybook arg types value - color and size

---
+++ packages/shared/src/constants/storybook/arg-types.ts
@@ -0,0 +1,12 @@
+import type { SharedColorProp, SharedSizeProp } from '@/types'
+
+export const SHARED_COLOR_VALUES: SharedColorProp[] = [
+  'default',
+  'primary',
+  'secondary',
+  'success',
+  'warning',
+  'danger',
+]
+
+export const SHARED_SIZE_VALUES: SharedSizeProp[] = ['small', 'medium', 'large']
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -4,5 +4,8 @@ export { clsx } from 'clsx'
 // export shared utilities
 export * from './utilities'
 
+// export shared constants
+export * from './constants'
+
 // export shared types
 export type * from './types'
