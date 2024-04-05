5db29cc32276c55f4ba5303bc5434b3949368d7c
plasticine <975036719@qq.com>
2024-04-05 21:47:52 +0800

    feat(shared): add shared props - SharedSizeProp

--- packages/shared/src/types/props.ts
+++ packages/shared/src/types/props.ts
@@ -2,3 +2,8 @@
  * The color prop shared with components.
  */
 export type SharedColorProp = 'default' | 'primary' | 'secondary' | 'success' | 'warning' | 'danger'
+
+/**
+ * The size prop shared with components.
+ */
+export type SharedSizeProp = 'small' | 'medium' | 'large'
